b8c989eb7f8c183c77ddfb74c3f35b18e64bc246
Rafael Couto <rafaelcouto58@gmail.com>
2024-10-13 15:03:04 -0300

    Atualização do README.md e do index.js

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 // Variável para herói utilizando lista (uma vez que objetos ainda não foram abordados no bootcamp)
 //              Nome        XP
 const heroi = ["Légolas", 13000];
+
 // Variável para armazenar os títulos disponíveis
 //              [0]        [1]       [2]      [3]      [4]         [5]          [6]        [7]
 const niveis = ["Ferro", "Bronze", "Prata", "Ouro", "Platina", "Ascendente", "Imortal", "Radiante"];
 
+ // Estrutura de controle para classificar o herói conforme sua XP, com a saída dos dados pelo Console.log
+ // Embora a utilização de objetos e funções pudesse otimizar o processo, e tornálo reutilizável, optei por me ater ás ferramentas que já foram vistas no bootcamp.
 if (heroi[1] < 1000) {
     console.log(`O Herói de nome ${heroi[0]} está no nível de ${niveis[0]}.`);
 } else if (heroi[1] < 2000) {
